989780a90dc8a4bed288c2e5cb6a142d1e3e7133
Ismerie George <ismerie.george@gmail.com>
2025-02-27 16:38:30 +0100

    feat: listProject fetch detail project, display icon

--- src/Profile/ProjectsList.js
+++ src/Profile/ProjectsList.js
@@ -1,32 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useStudent } from '../Utils/studentContext';
-import { screenHeight, screenWidth } from '../Utils/constant';
+import { screenHeight, screenWidth, apiClient } from '../Utils/constant';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function ProjectsList() {
     const { student } = useStudent();
     const [loading, setLoading] = useState(true);
     const [filteredProjects, setFilteredProjects] = useState([]);
+    const [projectDetails, setProjectDetails] = useState({}); // Stocke les détails récupérés
+
+    useEffect(() => {
+        if (student) {
+            setLoading(false);
+            const filtered = student.projects_users.filter(
+                item => item.marked === true && item.cursus_ids[0] === 21
+            );
+            setFilteredProjects(filtered);
+            fetchProjectDetails(filtered);
+        }
+    }, [student]);
+
+    const fetchProjectDetails = async (projects) => {
+        const details = {};
+        for (const project of projects) {
+            try {
+                const res = await apiClient.get(`/projects/${project.project.id}`);
+                if (res.status !== 200) throw new Error('Erreur API 42');
+                
+                details[project.project.id] = {
+                    exam: res.data.exam,
+                    solo: res.data.project_sessions?.[0]?.solo || false,
+                };
+
+                await new Promise(resolve => setTimeout(resolve, 500)); // Attendre 0.5s entre chaque requête
+            } catch (error) {
+                console.error(error);
+                break ;
+            }
+        }
+        setProjectDetails(details);
+    };
+
+    const getProjectIcon = (projectId) => {
+        if (!projectDetails[projectId]) return "person-outline"; 
+        if (projectDetails[projectId].exam) return "school-outline";
+        if (projectDetails[projectId].solo) return "person-outline";
+        return "people-outline";
+    };
+
+    if (loading) {
+        return <Text>Chargement des données...</Text>;
+    }
 
     const calculateElapsedTime = (apiDate) => {
         if (!apiDate) return "Date inconnue";
+    
         let timeString = "";
         const givenDate = new Date(apiDate);
         const now = new Date();
     
-        // en millisecondes
         const diffInMs = now - givenDate;
-    
-        // Fonction de calcul de la différence
+        
         const calculateDiff = (timeUnit) => Math.floor(diffInMs / timeUnit);
-    
-        // en millisecondes, minutes, heures, jours, mois, années
+        
         const diffInMinutes = calculateDiff(1000 * 60);
         const diffInHours = calculateDiff(1000 * 60 * 60);
         const diffInDays = calculateDiff(1000 * 60 * 60 * 24);
         const diffInMonths = calculateDiff(1000 * 60 * 60 * 24 * 30);
         const diffInYears = calculateDiff(1000 * 60 * 60 * 24 * 30 * 12);
-
+    
+        
         switch (true) {
             case (diffInYears >= 1):
                 timeString = diffInYears === 1 ? "1 year" : `${diffInYears} years`;
@@ -42,29 +86,13 @@ export default function ProjectsList() {
                 break;
             case (diffInMinutes >= 1):
                 timeString = diffInMinutes === 1 ? "1 minute" : `${diffInMinutes} minutes`;
-                break;    
+                break;
             default:
                 return "then less 1 minute ago";
         }
     
-        return `about ${timeString} ago`;
+        return `about ${timeString} ago`; 
     };
-    
-    
-    useEffect(() => {
-        if (student) {
-            setLoading(false);
-            console.log(student.projects_users[0]);
-            const filtered = student.projects_users.filter(item => item.marked === true && item.cursus_ids[0] === 21);
-            setFilteredProjects(filtered);
-            console.log(filtered);
-            console.log(filtered[0]);
-        }
-    }, [student]);
-
-    if (loading) {
-        return <Text>Chargement des données...</Text>;
-    }
 
     return (
         <View style={styles.container}>
@@ -73,11 +101,22 @@ export default function ProjectsList() {
                 keyExtractor={item => item.id.toString()}
                 renderItem={({ item }) => (
                     <View style={styles.card}>
-                        <View>
+                        <Icon
+                            name={getProjectIcon(item.project.id)}
+                            size={34}
+                            color="black"
+                            style={styles.typeProject}
+                        />
+                        <View style={styles.nameContainer}>
                             <Text style={styles.nameProject}>{item.project.name}</Text>
                             <Text style={styles.timeProject}>{calculateElapsedTime(item.marked_at)}</Text>
                         </View>
-                        <Text>{item.final_mark}</Text>
+                        <View style={styles.markContainer}>
+                            <Text style={styles.mark}>{item.final_mark}</Text>
+                            {!item.project.validated && (
+                                <Icon name="close-outline" size={24} color="red" style={styles.iconMark}/>
+                            )}
+                        </View>
                     </View>
                 )}
             />
@@ -92,14 +131,42 @@ const styles = StyleSheet.create({
     card: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        paddingLeft: screenWidth * 0.08,
-        paddingRight: screenWidth * 0.08,
-        marginBottom: screenHeight * 0.009
+        alignItems: 'center',
+        paddingLeft: screenWidth * 0.04,
+        paddingRight: screenWidth * 0.04,
+        paddingTop: screenHeight * 0.015,
+        paddingBottom: screenHeight * 0.015,
+        marginBottom: screenHeight * 0.02,
+        borderWidth: 2,
+        borderColor: '#edede9',
+        marginRight: 20, 
+        marginLeft: 20,
+        borderTopRightRadius: 10, 
+        borderBottomStartRadius: 10,
+        backgroundColor: '#edede9'
+    },
+    nameContainer: {
+        flex: 1,
+        justifyContent: 'start',
     },
     nameProject: {
-
+        fontSize: 18,
+        flexWrap: 'wrap',
+        maxWidth: screenWidth * 0.6,
+        marginBottom: screenHeight * 0.01,
+    },
+    mark: {
+        fontSize: 20,
+    },
+    typeProject: {
+        marginRight: screenWidth * 0.04,
+    },
+    markContainer: {
+        flexDirection: 'row', 
+        alignItems: 'end',
+    },
+    iconMark: {
     },
     timeProject: {
-
     },
 });
